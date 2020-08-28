5ed2589c02e5f5f3fbae030e053429854e4f7b03
André Moraes <amancioandre@gmail.com>
2020-08-28 15:48:48 -0300

    feat: add template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+    constructor(private readonly mailerService: MailerService) {}
+
+    async sendMail(text: string): Promise<void> {
+        this
+        .mailerService
+        .sendMail({
+          to: 'aademora@mtu.edu',
+          from: 'noreply@nestjs.com',
+          subject: 'Testing Nest Mailermodule with template ✔',
+          template: __dirname + '../templates/index', // The `.pug`, `.ejs` or `.hbs` extension is appended automatically.
+          context: {  // Data to be sent to template engine.
+            text
+          },
+        })
+        .then(() => {})
+        .catch(() => {});
+    }
+
+}
