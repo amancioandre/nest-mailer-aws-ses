f714fecb6beb39ca8d216048a72ea00c4f02106e
André Moraes <amancioandre@gmail.com>
2020-08-28 15:40:26 -0300

    feat: enable .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailModule } from './mail/mailer.module';
+import { ConfigModule} from '@nestjs/config'
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     MailModule
   ],
   controllers: [AppController],
